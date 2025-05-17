faef758ed04769608bbde6d6217818af70976c8c
SainiLikith <sainilikith@gmail.com>
2025-05-18 01:48:49 +0530

    latest

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -30,6 +30,7 @@ function App() {
   return (
     <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} />
             <Route path="/register" element={<Register />} />
             <Route
